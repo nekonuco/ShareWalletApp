335b45da12fc8dc4f0d779d8a76539dcadcdb955
TajimaSayuri <hinaduki25@gmail.com>
2022-07-24 15:05:31 +0900

    Login画面仮レイアウト

--- ShareWalletApp/ViewController/LoginViewController.swift
+++ ShareWalletApp/ViewController/LoginViewController.swift
@@ -8,29 +8,37 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+        
+    /* 定義 */
+    //ロゴイメージ
+    @IBOutlet weak var appTrademark: UIImageView!
+    //ログインボタン
+    @IBOutlet weak var loginBtn: UIButton!
+    //パスワードリセット
+    @IBOutlet weak var passwordReset: UIButton!
     
-    @IBOutlet weak var serviceName: UILabel!
-
+    //NSAttributedString
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    //** 仮のデザイン設定なので、自由に変えてください　by tajima **//
+        //ログインボタンのデザイン設定
+        loginBtn.layer.cornerRadius = 20
+        
+        //パスワードリセットボタンのデザイン設定
+        passwordReset.setTitle("パスワードを忘れた方はこちら", for: .normal)
+        
     }
     
-    
-    
+    //ログインボタン押下時
     @IBAction func loginBtn(_ sender: Any) {
-        
-        // テスト
-        
         self.performSegue(withIdentifier: "secceedLogin", sender: nil)
     }
     
+    //パスワードリセットボタン押下時
     @IBAction func passwordReset(_ sender: Any) {
-        
         self.performSegue(withIdentifier: "passwordResetSegue", sender: nil)
     }
-    
-
-
-
 }
